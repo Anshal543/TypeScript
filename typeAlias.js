7df26917226449a30b8052098eb0a127cd639681
Anshal543 <sherhasan543@gmail.com>
2024-06-17 22:31:18 +0500

    type alias

--- typeAlias.js
+++ typeAlias.js
@@ -1,2 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// type myString = string
+function createUser(user) {
+    return { name: user.name, email: user.email, isAdmin: user.isAdmin };
+}
+var userResult = createUser({ name: 'John', email: 'anshal@gmail.com', isAdmin: true });
+console.log(userResult);
--- typeAlias.ts
+++ typeAlias.ts
@@ -1,5 +1,17 @@
 
+type User = {
+    name: string,
+    email: string,
+    isAdmin: boolean
+}
 
+// type myString = string
 
+function createUser(user:User):User{
+    return {name: user.name, email: user.email, isAdmin: user.isAdmin}
+}
+
+const userResult = createUser({name: 'John', email: 'anshal@gmail.com', isAdmin: true})
+console.log(userResult);
 
 export {}
